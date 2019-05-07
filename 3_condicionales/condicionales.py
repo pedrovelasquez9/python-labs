bbb9911c66fece9a7fc4482083a24da0edea6f09
Pedro Plasencia <pjpv9011@gmail.com>
2019-05-07 23:44:35 +0200

    condicionales

---
+++ 3_condicionales/condicionales.py
@@ -0,0 +1,21 @@
+a = 10;
+b = 5;
+
+#No hay switch así que podemos poner tantos elif como necesitemos
+#condicional if
+
+if(a<b) :
+	print("a es menor que b");
+elif(a==b):
+	print("a es igual a b");
+else:
+	print("a es mayor que b");
+
+#Búsqueda con if in
+
+lista = ['lunes', 'martes', 'miercoles'];
+if "martes" in lista : 
+	print('si está el martes');
+else:
+	print("No está el martes");
+
